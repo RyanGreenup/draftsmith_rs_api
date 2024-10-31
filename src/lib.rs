6f206e861da41dc45c23317db30970767eceb064
ryan <exogenesis@protonmail.com>
2024-10-31 16:04:56 +1100

    feat: add Note and NewNote structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::notes)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Note {
+    pub id: i32,
+    pub title: String,
+    pub content: String,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+    pub search_vector: Option<diesel::sql_types::Tsvector>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::notes)]
+pub struct NewNote<'a> {
+    pub title: &'a str,
+    pub content: &'a str,
 }
 
 #[cfg(test)]
