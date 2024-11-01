7db07ddca002742aaf2fda3ffc3c7e3fb0268689
ryan <exogenesis@protonmail.com>
2024-11-01 12:00:41 +1100

    added the tables
    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,12 @@
-pub mod assets;
-pub mod attributes;
-pub mod journal_entries;
-pub mod note_attributes;
-pub mod note_hierarchy;
-pub mod note_modifications;
-pub mod note_tags;
-pub mod note_type_mappings;
-pub mod note_types;
-pub mod notes;
 pub mod schema;
-pub mod tag_hierarchy;
-pub mod tags;
 
 use diesel::deserialize::{FromSql, Result};
 use diesel::pg::{Pg, PgValue};
+use diesel::prelude::*;
 use diesel::serialize::{IsNull, Output, ToSql};
 use diesel::{AsExpression, FromSqlRow};
+use schema::*;
 use std::io::Write;
-use diesel::prelude::*;
-
 
 #[derive(Debug, Clone, AsExpression, FromSqlRow)]
 #[diesel(sql_type = crate::schema::sql_types::Tsvector)]
@@ -292,37 +280,4 @@ pub struct NewTask<'a> {
     pub modified_at: Option<chrono::NaiveDateTime>,
     pub all_day: Option<bool>,
     pub goal_relationship: Option<i32>,
-}
-
-#[cfg(test)]
-pub mod test_utils {
-    use diesel::pg::PgConnection;
-    use diesel::Connection;
-    use dotenv::dotenv;
-    use std::env;
-    use crate::schema::tags;
-    use crate::tags::NewTag;
-    use crate::tags::Tag;
-    use crate::schema::tags::dsl::*;
-
-    pub fn establish_test_connection() -> PgConnection {
-        dotenv().ok();
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        PgConnection::establish(&database_url).expect("Error connecting to database")
-    }
-
-    fn setup_default_data(conn: &mut PgConnection) -> Result<(), diesel::result::Error> {
-        // Insert some default tags into the database
-        let new_tags = vec![NewTag { name: "Tag1" }, NewTag { name: "Tag2" }];
-        let mut inserted_tags = Vec::with_capacity(new_tags.len());
-
-        for new_tag in new_tags.iter() {
-            // Insert the tag
-            let inserted_tag: Tag = diesel::insert_into(tags)
-                .values(new_tag.clone())
-                .get_result(conn)?;
-        }
-
-        Ok(())
-    }
 }
