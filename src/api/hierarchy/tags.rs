682269cf93706a677c1d198e5a3411fbe6aaeef5
ryan <exogenesis@protonmail.com>
2024-11-06 13:17:16 +1100

    feat: Add hierarchy tags API

---
+++ src/api/hierarchy/tags.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct TagTreeNode {
+    pub id: i32,
+    pub name: String,
+    pub children: Vec<TagTreeNode>,
+}
